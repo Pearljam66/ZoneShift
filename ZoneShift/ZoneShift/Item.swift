4ce026213635e1e1505ebcbfc8f976c7e6d763aa
Sarah <sarahireneclark@gmail.com>
2025-03-16 16:58:34 -0500

    Initial project codef

---
+++ ZoneShift/ZoneShift/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ZoneShift
+//
+//  Created by Sarah Clark on 3/16/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ ZoneShift/ZoneShiftTests/ZoneShiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  ZoneShiftTests.swift
+//  ZoneShiftTests
+//
+//  Created by Sarah Clark on 3/16/25.
+//
+
+import Testing
+@testable import ZoneShift
+
+struct ZoneShiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
